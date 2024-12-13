6aa93394c7712f49b786c1012dffdd24ca38745a
F1N3X <quartzman92@gmail.com>
2024-12-13 09:32:59 +0100

    error page

---
+++ error.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>🤔 What did you mean ?</h2>
+        <p>I'm sorry but all I can guive you is this :</p>
+        <h1>404</h1>
+        <p>Come on, you can do it, try again 😉</p>
+    </div>
+</template>
