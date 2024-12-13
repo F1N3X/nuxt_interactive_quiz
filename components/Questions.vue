6ea13d51c2c4d7c48333df695dce659ea001009b
F1N3X <quartzman92@gmail.com>
2024-12-13 15:37:58 +0100

    quiz questions style

--- components/Questions.vue
+++ components/Questions.vue
@@ -5,56 +5,63 @@
         quizDatas.answer(value)
     }
 
-    function reset(){
+    function reset() {
         quizDatas.reset()
     }
-
 </script>
 
-
-
 <template>
-    <div>
+    <div class="pt-10 px-5">
         <div v-if="quizDatas.quiz.step-1 < quizDatas.quiz.questionNumber">
-
-            <h1>Question : {{ quizDatas.quiz.quizData.questions[quizDatas.quiz.step-1].text }} </h1>
+            <h1 class="text-xl font-bold mb-4 pb-14">Question: {{ quizDatas.quiz.quizData.questions[quizDatas.quiz.step-1].text }}</h1>
 
             <div>
-                <div v-for="(answer, index) in quizDatas.quiz.quizData.questions[quizDatas.quiz.step-1].answers" :key="index">
-                    <input 
-                        type="submit" 
-                        :value="answer.text" 
-                        @click="answerQuestion(answer.isCorrect)"
-                    />
-                </div>
-
-                <div>
-                        Question {{ quizDatas.quiz.step }} / {{ quizDatas.quiz.questionNumber }}
-                </div>
-
-                <div>
-                    <input 
-                        type="submit" 
-                        value="Reset" 
-                        @click="reset()"
-                    />
-
-                    <NuxtLink 
-                    :to="`/`" 
+                <!-- Grille des réponses -->
+                <div 
+                    class="grid grid-cols-2 gap-4 w-full justify-items-center"
+                >
+                    <div 
+                        v-for="(answer, index) in quizDatas.quiz.quizData.questions[quizDatas.quiz.step-1].answers" 
+                        :key="index" 
+                        class="flex justify-center w-full"
                     >
-                    <input type="submit" value="Other quiz" />
-                </NuxtLink>
+                        <button 
+                            @click="answerQuestion(answer.isCorrect)" 
+                            class="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-lg w-3/4"
+                        >
+                            {{ answer.text }}
+                        </button>
+                    </div>
+                </div>
+
+                <div class="mt-4 text-center pt-20">
+                    Question {{ quizDatas.quiz.step }} / {{ quizDatas.quiz.questionNumber }}
+                </div>
+
+                <div class="mt-4 flex justify-between">
+                    <button 
+                        @click="reset()" 
+                        class="bg-red-500 text-white px-4 py-2 rounded-lg cursor-pointer"
+                    >
+                        Reset
+                    </button>
+
+                    <NuxtLink :to="`/`">
+                        <button class="bg-green-500 text-white px-4 py-2 rounded-lg cursor-pointer">
+                            Other quiz
+                        </button>
+                    </NuxtLink>
                 </div>
             </div>
         </div>
 
-        <div v-else>
-            <h1>Quiz is over</h1>
-            <h2>Your score is {{ quizDatas.quiz.score }} / {{ quizDatas.quiz.questionNumber * 5 }}</h2>
-            <NuxtLink 
-                :to="`/`" 
-            >
-                <input type="submit" value="Back to home" />
+        <div v-else class="w-full flex flex-col items-center justify-center">
+            <h1 class="text-2xl font-bold mb-4">Quiz is over</h1>
+            <h2 class="text-xl mb-4">Your score is {{ quizDatas.quiz.score }} / {{ quizDatas.quiz.questionNumber * 5 }}</h2>
+            <NuxtLink :to="`/`">
+                <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer">
+                    Back to home
+                </button>
             </NuxtLink>
         </div>
     </div>
